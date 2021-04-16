e7216e9790d3c946a90c66b18bb9cc3ab9471765
mbkoltai <kolmisi@gmail.com>
2021-04-16 12:15:40 +0100

    refresh gitignore

--- data/somalia/Mogadishu_data/mogadishu_burial_analysis-main/reshape_ocha_data.R
+++
@@ -1,8 +0,0 @@
-  ocha <- read_excel("banadir_covid_data.xlsx", sheet = "ocha")
-    # remove tibble nonsense
-    ocha <- as.data.frame(ocha)
-
-    
-a <- melt(ocha, id = c("date", "variable"))    
-colnames(a) <- c("date", "variable", "cemetery", "deaths")
-write.csv(a, "a.csv", row.names = F)
